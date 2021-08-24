a21b1cd2ecd038ca92da4c7adc94a7634c6a3752
Jess Pesale <jess_pesale@aol.com>
2021-08-23 23:39:44 -0400

    commit tree 224ca553d472f8b4597543c3d63eed32cc8a1002

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+
+function map(array, funct) {
+    let x = []
+    for (let i = 0; i < array.length; i++) {
+        let elem = array[i]
+        x.push(funct(elem))
+    }
+    return x
+}
+
+function reduce(array, funct, start) {
+    let total  
+        if (start) {
+            total = start
+           for (let i = 0; i < array.length; i++) {
+               total = funct(array[i], total)
+           }
+        }
+        else {
+            total = array[0]
+           for (let i = 1; i < array.length; i++) {
+               total = funct(array[i], total)
+           }
+        }
+            return total
+}
